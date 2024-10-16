265b033c8c7717ce54b7b5e166c2425ba41da136
dknific <dknific@gmail.com>
2024-10-16 14:49:52 -0700

    004 - Externalized API key

--- src/main/java/com/simpsonfans/simpsons_list/service/AuthenticationService.java
+++ src/main/java/com/simpsonfans/simpsons_list/service/AuthenticationService.java
@@ -1,23 +1,41 @@
 package com.simpsonfans.simpsons_list.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Service;
 
 import com.simpsonfans.simpsons_list.security.ApiKeyAuthentication;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 
+@Service
 public class AuthenticationService {
 	private static final String AUTH_TOKEN_HEADER_NAME = "x-api-key";
-	private static final String AUTH_TOKEN = "Baeldung";
+	
+	private String apiKeyValue;
+	private static String API_KEY;
+	
+	@Value("${api.key}")
+	public void setApiKey(String apiKey) {
+		this.apiKeyValue = apiKey;
+	}
+	
+	@PostConstruct
+	public void initializeApiKey() {
+		API_KEY = this.apiKeyValue;
+	}
 	
 	public static Authentication getAuthentication(HttpServletRequest request) {
-		String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
-		if (apiKey == null || !apiKey.equals(AUTH_TOKEN)) {
+		String requestApiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+		System.out.println("Does " + requestApiKey + " match " + API_KEY);
+		
+		if (requestApiKey == null || !requestApiKey.equals(API_KEY)) {
 			throw new BadCredentialsException("Invalid API Key");
 		}
 		
-		return new ApiKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
+		return new ApiKeyAuthentication(requestApiKey, AuthorityUtils.NO_AUTHORITIES);
 	}
 }
